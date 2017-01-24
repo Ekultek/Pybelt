d76073e66e70758a43146da6d5da1d02205d76fd
Ekultek <thomas.perkins23@icloud.com>
2017-01-24 14:25:05 -0600

    edited a lot of settings and changed banner

--- lib/core/settings.py
+++ lib/core/settings.py
@@ -3,6 +3,8 @@ import uuid
 import re
 import logging
 import random
+import time
+import urllib2
 import base64
 from colorlog import ColoredFormatter
 
@@ -21,30 +23,34 @@ LOGGER.addHandler(stream)
 PATH = os.getcwd()
 
 # Current version
-VERSION = "1.0.1"
+VERSION = "1.1.2"
 
 # Coloring for the version string
 TYPE_COLORS = {"dev": 33, "stable": 92}
 
 # Type of version that the program is in "dev" or "stable"
-VERSION_STRING = "\033[92m{}\033[0m(\033[{}m\033[1mdev\033[0m)".format(VERSION, TYPE_COLORS["dev"]) if len(VERSION) >= 4 else \
+VERSION_STRING = "\033[92m{}\033[0m(\033[{}m\033[1mdev\033[0m)".format(VERSION, TYPE_COLORS["dev"]) if len(
+    VERSION) >= 4 else \
     "\033[92m{}\033[0m(\033[{}m\033[1mstable\033[0m)".format(VERSION, TYPE_COLORS["stable"])
 
 # Clone link
 CLONE_LINK = "https://github.com/ekultek/pybelt.git"
 
 # Basic legal disclaimer
-LEGAL_DISC = "[!] legal disclaimer: This program is intended for learning purposes, any malicious intent is on you, it is the end users responsibility to obey all laws, regulations, and rules of your respective country or place of origin. For further information about this please see the legal information file under docs or run the --legal flag"
-LONG_LEGAL_DISCLAIMER = open("lib/core/text_files/legal.txt").read()
-
-# Random dork to use for basic sqli searches
-# RANDOM_DORK = random.choice(open("{}/lib/core/text_files/dorks.txt".format(PATH)).readlines())
+LEGAL_DISC = "[!] legal disclaimer: This program is intended for learning purposes, any malicious intent is on you, " \
+             "it is the end users responsibility to obey all laws, regulations, and rules of your respective country " \
+             "or place of origin. For further information about this please see the legal information file under docs " \
+             "or run the --legal flag"
+LONG_LEGAL_DISCLAIMER = open("lib/text_files/legal.txt").read()
 
 # Random saying to display on the banner
-SAYING = "The Hackers ToolBelt.."
+SAYING = random.choice(["The Hackers ToolBelt..",
+                        "The Hackers Blackbelt..",
+                        "The Hackers Multi-Tool..",
+                        "The Hackers Gerber.."])
 
 # Random common column names
-RANDOM_COMMON_COLUMN = random.choice(open("{}/lib/core/text_files/common_columns.txt".format(PATH)).readlines())
+RANDOM_COMMON_COLUMN = random.choice(open("{}/lib/text_files/common_columns.txt".format(PATH)).readlines())
 
 # Search query regex to make sure the URLS have a GET parameter
 QUERY_REGEX = re.compile(r"(.*)[?|#](.*){1}\=(.*)")
@@ -61,29 +67,18 @@ SYNTAX_REGEX = re.compile(r"\W+$")
 IP_ADDRESS_REGEX = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 
 # Sexy ass banner
-BANNER = """
-        \033[94m##### ##                    /              ###
-     ######  /###                 #/                ###
-    /#   /  /  ###                ##                 ##     #
-   /    /  /    ###               ##                 ##    ##
-       /  /      ##               ##                 ##    ##
-      ## ##      ## ##   ####     ## /###     /##    ##  ########
-      ## ##      ##  ##    ###  / ##/ ###  / / ###   ## ########
-    /### ##      /   ##     ###/  ##   ###/ /   ###  ##    ##
-   / ### ##     /    ##      ##   ##    ## ##    ### ##    ##
-      ## ######/     ##      ##   ##    ## ########  ##    ##
-      ## ######      ##      ##   ##    ## #######   ##    ##
-      ## ##          ##      ##   ##    ## ##        ##    ##
-      ## ##          ##      ##   ##    /# ####    / ##    ##
-      ## ##           #########    ####/    ######/  ### / ##
- ##   ## ##             #### ###    ###      #####    ##/   ##
-###   #  /                    ###
- ###    /              #####   ###
-  #####/             /#######  /#
-    ###             /      ###/\033[0m      %s
-
+BANNER = """\033[94m
+  |                          /_/    |
+- * -                       /_/   - * -           /_/    /_/
+  |  /_/_/_/    /_/   /_/  /_/_/_/  |    _/_/    /_/  /_/_/_/_/    |
+    /_/    _/  /_/   /_/  /_/    _/  /_/_/_/_/  /_/    /_/       - * -
+   /_/    _/  /_/   /_/  /_/    _/  /_/        /_/    /_/          |
+  /_/_/_/      _/_/_/   /_/_/_/_/   /_/_/_/   /_/    /_/_/
+ /_/              _/
+/_/     |     /_/_/    %s
+      \033[94m- * -
+        |\033[0m
 \033[94m%s
-
 %s\033[0m\n\n
 """ % (VERSION_STRING, SAYING.strip(), CLONE_LINK)
 
@@ -100,11 +95,24 @@ GOOGLE_TEMP_BLOCK_ERROR_MESSAGE += "\tD) Curse my name and this program"
 
 # List of reserved port numbers, these are the ports that you want to check
 RESERVED_PORTS = {
-    1,   5,   7,   18,  20,  21,  22,  23,  25,  29,  37,  42,  43,  49,
-    53,  69,  70,  79,  80,  103, 108, 109, 110, 115, 118, 119, 137, 139,
+    1, 5, 7, 18, 20, 21, 22, 23, 25, 29, 37, 42, 43, 49,
+    53, 69, 70, 79, 80, 103, 108, 109, 110, 115, 118, 119, 137, 139,
     143, 150, 156, 161, 179, 190, 194, 197, 389, 396, 443, 444, 445, 458,
     546, 547, 563, 569, 1080
 }
+
+# Links to some wordlists I have laying around
+WORDLIST_LINKS = [
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrL2FhODgyMDk5ZWQxYzNlZjAwNWYzYWY2ZjhmYmFhZTExL3Jhdy84ODQ4NjBhNjAzZWQ0MjE3MTgyN2E1MmE3M2VjNzAzMjNhOGExZWY5L2dpc3RmaWxlMS50eHQ=',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrLzAwNWU3OWQ2NmU2MzA2YWI0MzZjOGJmYTc1ZTRiODMwL3Jhdy8xNjY5YjNjMDFmMjRhM2Q2OTMwZDNmNDE1Mjk3ZTg5OGQ1YjY2NGUzL29wZW53YWxsXzMudHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrLzE4NTBmM2EwZGNjNDE0YWZlOGM3NjYyMjBlOTYxYjE4L3Jhdy9iYWQ0NTA0NjcwY2FmM2UxNDY1NWI2ZjJlZGQ0MjJmOTJjMzI2MWI5L215c3BhY2UudHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrLzBkYWU2YTI5MjgzMjcyNmE2Y2MyN2VlNmVjOTdmMTFjL3Jhdy84MWFkOWFkOWUwZjQxMmY2YjIwMTM3MDI2NDcxZGRmNDJlN2JjMjkyL2pvaG4udHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrL2Q4ZjZiYjE2MGEzYzY2YzgyNWEwYWY0NDdhMDM1MDVhL3Jhdy83MWI4NmM5MGU3NDRkZjM0YzY3ODFjM2U0MmFjMThkOGM4ZjdkYjNlL2NhaW4udHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrL2JmM2MwYjQwMTVlYzlkMzY4YzBlNTczNzQ0MTAzYmU1L3Jhdy9lNzBhMThmOTUwNGYwZmMyYjRhMWRmN2M0Mjg2YjcyOWUyMzQ5ODljL29wZW53YWxsXzIudHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrLzQ1ZTExZDBhMzNjZGE1YjM3NDM5OGYyMDgxYjEwZWZiL3Jhdy8wNzQ1ZGMzNjFlZDU5NjJiMjNkYjUxM2FkOWQyOTNlODk0YjI0YTY0L2RjLnR4dA==',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrLzNmMzcxMWUzMDdlOGM0ZTM0MDkzYzI1OGFkN2UzZWZkL3Jhdy9hMjNiYmM3YTgxNTZhOGU5NTU3NmViYTA3MmIwZDg4ZTJmYjk1MzZiL2dtYWlsXzIudHh0',
+    'aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9Fa3VsdGVrL2U3MzE4MGM3MGZmMzY3NDFhM2M4NzIzMDZiNTFhOTU1L3Jhdy9jODE0YjFjOTZiNGJkYzZlYTRlZDE3MmMzNDIwOTg2NTBjOTcyYWZjL2J0NC50eHQ='
+]
 
 
 def create_random_filename():
@@ -117,3 +125,19 @@ def create_random_filename():
 def decode64(string):
     """ Decode a string from base64 """
     return base64.b64decode(string)
+
+
+def prompt(question):
+    """ Ask a question.. """
+    return raw_input("[{} PROMPT] {}".format(time.strftime("%I:%M:%S"), question))
+
+
+def create_wordlist(b64link):
+    """ Create a word list from a base64encoded URL by decoding it and connecting to it"""
+    path = "{}/lib/text_files/wordlist.txt".format(PATH)
+    data = urllib2.urlopen(base64.b64decode(b64link)).read()
+    open(path, "w").close()
+    with open(path, 'a+') as wordlist:
+        for word in data:
+            wordlist.write(word)
+    return
