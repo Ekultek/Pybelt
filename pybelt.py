aaf133effb54188db5b22d7983884684e810ad76
Ekultek <thomas.perkins23@icloud.com>
2017-01-24 14:25:32 -0600

    new flags

--- pybelt.py
+++ pybelt.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import re
+import random
 from urllib2 import HTTPError
 from lib.core import BANNER
 from lib.core.errors import GoogleBlockException
@@ -8,31 +9,33 @@ from lib.core.settings import LOGGER
 from lib.core.settings import LEGAL_DISC
 from lib.core.settings import QUERY_REGEX
 from lib.core.settings import VERSION_STRING
+from lib.core.settings import create_wordlist
+from lib.core.settings import WORDLIST_LINKS
 from lib.core.settings import IP_ADDRESS_REGEX
 from lib.core.settings import LONG_LEGAL_DISCLAIMER
 from lib.core.settings import GOOGLE_TEMP_BLOCK_ERROR_MESSAGE
 from lib.core.sql_scan import SQLiScanner
 from lib.core.dork_check import DorkScanner
 from lib.core.port_scan import PortScanner
+from lib.core.hash_cracking import HashCracker
 
 if __name__ == '__main__':
     opts = argparse.ArgumentParser()
     opts.add_argument('-d', '--dork-check', metavar='DORK', dest="dorkcheck",
                       help="Provide a Google dork to check for possible injectable sites")
-    opts.add_argument('-c', '--hash-cracker', metavar="HASH", dest="hashcracking",
-                      help=argparse.SUPPRESS)
-    opts.add_argument('-p', '--port-scanner', metavar="HOST", dest="portscan",
+    opts.add_argument('-c', '--hash-crack', metavar="HASH", dest="hash", nargs=1,
+                      help="Specify a hash to crack and a hash type, IE: -c <HASH>:md5 (default all)")
+    opts.add_argument('-p', '--port-scan', metavar="HOST", dest="portscan",
                       help="Provide a host to scan for open ports")
     opts.add_argument('-s', '--sqli-scanner', metavar="URL", dest="sqliscan",
                       help="Provide a URL to scan for SQL injection flaws")
-    opts.add_argument('-D', '--dork-file', metavar="DORK FILE", dest="dorkfilecheck",
-                      help=argparse.SUPPRESS)
-    opts.add_argument('-C', '--hash-file', metavar="HASHFILE", dest="hashfilecracking",
-                      help=argparse.SUPPRESS)
+
     opts.add_argument('-l', '--legal', action="store_true", dest="legal",
                       help="Display the legal information")
     opts.add_argument('--version', action="store_true", dest="version",
                       help="Show the version number and exit")
+    opts.add_argument('--rand-wordlist', action="store_true", dest="random_wordlist",
+                      help="Use a random wordlist from a Gist link")
     args = opts.parse_args()
 
     print(BANNER + "\033[91m{}\033[0m".format(LEGAL_DISC) + "\n") if args.legal is False else \
@@ -43,9 +46,14 @@ if __name__ == '__main__':
             LOGGER.info(VERSION_STRING)
             sys.exit(0)
 
+        if args.random_wordlist is True:
+            LOGGER.info("Creating a random wordlist..")
+            create_wordlist(random.choice(WORDLIST_LINKS))
+            LOGGER.info("Wordlist created, resuming process..")
+
         if args.sqliscan is not None:
             if QUERY_REGEX.match(args.sqliscan):
-                LOGGER.info("Starting SQLi scan on {}".format(args.sqliscan))
+                LOGGER.info("Starting SQLi scan on {}..".format(args.sqliscan))
                 LOGGER.info(SQLiScanner(args.sqliscan).sqli_search())
             else:
                 LOGGER.error("URL does not contain a query (GET) parameter. Example: http://example.com/php?id=2")
@@ -58,13 +66,24 @@ if __name__ == '__main__':
                 LOGGER.fatal(GoogleBlockException(GOOGLE_TEMP_BLOCK_ERROR_MESSAGE))
                 exit(1)
 
-        if args.portscan and re.search(IP_ADDRESS_REGEX, sys.argv[2]) is not None:
+        if args.hash is not None:
+            try:
+                items = list(''.join(args.hash).split(":"))
+                if items[1] == "all":
+                    LOGGER.info("Starting hash cracking without knowledge of algorithm...")
+                    HashCracker(items[0]).try_all_algorithms()
+                    exit(0)
+                else:
+                    LOGGER.info("Starting hash cracking using %s as algorithm type" % items[1])
+                    HashCracker(items[0], type=items[1]).try_certain_algorithm()
+                    exit(0)
+            except IndexError:
+                error_message = "You must specify a hash type in order for this to word"
+                exit(1)
+
+        if args.portscan is not None and re.search(IP_ADDRESS_REGEX, sys.argv[2]):
             LOGGER.info("Starting port scan on {}".format(args.portscan))
             LOGGER.info(PortScanner(args.portscan).connect_to_host())
-        else:
-            LOGGER.fatal("You must provide a host (IP) to scan, example: 127.0.0.1")
 
     except KeyboardInterrupt:
         LOGGER.error("User aborted.")
-
-    print("Running with {}".format(sys.argv))
