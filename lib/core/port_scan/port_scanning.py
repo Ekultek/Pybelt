0ff056c7ec77171bfe13e5a617b19cc4aafa92a1
Ekultek <thomas.perkins23@icloud.com>
2017-01-25 13:04:15 -0600

    edited port scanner, it works better now

--- lib/core/port_scan/port_scanning.py
+++ lib/core/port_scan/port_scanning.py
@@ -1,4 +1,5 @@
 import socket
+import time
 from lib.core.settings import LOGGER
 from lib.core.settings import RESERVED_PORTS
 
@@ -12,20 +13,18 @@ class PortScanner(object):
         self.ports = RESERVED_PORTS
 
     def connect_to_host(self):
-        """ Attempt to make a connection using the most common ports 443, 445, etc.. """
-        host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        for port in self.ports:
-            try:
-                LOGGER.info("Attempting to connect to port: {}".format(port))
-                attempt = host.connect_ex((self.host, port))  # Connect to the host
-                if attempt:  # If connection fails
-                    pass
-                else:
+        start_time = time.time()
+        try:
+            for port in RESERVED_PORTS.keys():
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                res = sock.connect_ex((self.host, port))
+                if res == 0:
+                    LOGGER.info("[*] Open: {}  {}".format(port, RESERVED_PORTS[port]))
                     self.connection_made.append(port)
-            except socket.error:
-                pass
-        host.close()
-        if not self.connection_made:
-            LOGGER.fatal("No connections could be made.")
-        else:
-            return "Connection made on port: {}.".format(''.join(str(self.connection_made)))
+                sock.close()
+        except Exception, e:
+            print e
+
+        stop_time = time.time()
+        LOGGER.info("Completed in {} seconds".format(str(stop_time - start_time)))
+        LOGGER.info("Ports readily available: {}".format(''.join(str(self.connection_made))))
