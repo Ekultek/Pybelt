5aa69a13f0b3080d8e34d4fcd319e77193fe49aa
Ekultek <thomas.perkins23@icloud.com>
2017-02-08 08:40:13 -0600

    updated to show all possible hash types

--- lib/core/hash_checker/hash_check.py
+++ lib/core/hash_checker/hash_check.py
@@ -1,4 +1,4 @@
-import hashlib
+from lib.core.settings import prompt
 from lib.core.settings import LOGGER
 from lib.core.settings import HASH_TYPE_REGEX
 
@@ -8,12 +8,29 @@ class HashChecker(object):
     def __init__(self, check_hash):
         self.hash = check_hash
         self.found = False
-        self.hash_type = None
 
     def obtain_hash_type(self):
-        for alg in HASH_TYPE_REGEX.keys():
-            if HASH_TYPE_REGEX[alg].match(self.hash):
-                LOGGER.info("Possible hash type: {}".format(alg.upper()))
-                return
-        LOGGER.error("Unable to verify hash type for {}\nSupported hash types: {}".format(
-                     self.hash, hashlib.algorithms_available))
+        for algorithm in HASH_TYPE_REGEX:
+            if algorithm.match(self.hash):
+                self.found = True
+                self.enumerate_hash_types(HASH_TYPE_REGEX[algorithm])
+        if self.found is False:
+            error_message = "Unable to verify hash type "
+            error_message += "for hash: '{}'. This could mean ".format(self.hash)
+            error_message += "that this is not a valid hash, or that "
+            error_message += "this hash is not supported by Pybelt "
+            error_message += "yet. If you feel this should be supported "
+            error_message += "make an issue regarding this hash."
+            LOGGER.error(error_message)
+            return
+
+    @staticmethod
+    def enumerate_hash_types(items):
+        LOGGER.info("{} possible hash types found".format(len(items)))
+        count = 0
+        for item in items:
+            count += 1
+            if count <= 3:
+                print("\033[92m[*] Most likely possible hash type: {}\033[0m".format(item))
+            else:
+                print("\033[33m[*] Least likely possible hash type: {}\033[0m".format(item))
--- lib/core/settings.py
+++ lib/core/settings.py
@@ -23,7 +23,7 @@ LOGGER.addHandler(stream)
 PATH = os.getcwd()
 
 # Current version
-VERSION = "1.3.4"
+VERSION = "1.3.5"
 
 # Coloring for the version string
 TYPE_COLORS = {"dev": 33, "stable": 92}
@@ -113,13 +113,30 @@ URL_REGEX = re.compile(
 
 # Regex to match hash types, these are the only supported hashing types right now
 HASH_TYPE_REGEX = {
-    "md5": re.compile(r"^[a-f0-9]{32}(:.+)?$", re.IGNORECASE),
-    "sha256": re.compile(r"^[a-f0-9]{64}(:.+)?$", re.IGNORECASE),
-    "whirlpool": re.compile(r"^[a-f0-9]{128}(:.+)?$", re.IGNORECASE),
-    "sha224": re.compile(r"^[a-f0-9]{56}$", re.IGNORECASE),
-    "sha1": re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE),
-    "sha384": re.compile(r"^[a-f0-9]{96}$", re.IGNORECASE),
-    "sha512": re.compile(r"^[a-f0-9]{128}(:.+)?$", re.IGNORECASE),
+    re.compile(r"^[a-f0-9]{32}(:.+)?$", re.IGNORECASE):  ["MD5", "MD4", "MD2", "Double MD5",
+                                                          "LM", "RIPEMD-128", "Haval-128",
+                                                          "Tiger-128", "Skein-256(128)", "Skein-512(128",
+                                                          "Lotus Notes/Domino 5", "Skype", "ZipMonster",
+                                                          "PrestaShop"],
+    re.compile(r"^[a-f0-9]{64}(:.+)?$", re.IGNORECASE):  ["SHA-256", "RIPEMD-256", "SHA3-256", "Haval-256",
+                                                          "GOST R 34.11-94", "GOST CryptoPro S-Box",
+                                                          "Skein-256", "Skein-512(256)", "Ventrilo"],
+    re.compile(r"^[a-f0-9]{128}(:.+)?$", re.IGNORECASE): ["SHA-512", "Whirlpool", "Salsa10",
+                                                          "Salsa20", "SHA3-512", "Skein-512",
+                                                          "Skein-1024(512)"],
+    re.compile(r"^[a-f0-9]{56}$", re.IGNORECASE):        ["SHA-224", "Haval-224", "SHA3-224",
+                                                          "Skein-256(224)", "Skein-512(224)"],
+    re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE):  ["SHA-1", "Double SHA-1", "RIPEMD-160",
+                                                          "Haval-160", "Tiger-160", "HAS-160",
+                                                          "LinkedIn", "Skein-256(160)", "Skein-512(160)",
+                                                          "MangoWeb Enhanced CMS"],
+    re.compile(r"^[a-f0-9]{96}$", re.IGNORECASE):        ["SHA-384", "SHA3-384", "Skein-512(384)",
+                                                          "Skein-1024(384)"],
+    re.compile(r"^[a-f0-9]{16}$", re.IGNORECASE):        ["MySQL323", "DES(Oracle)", "Half MD5",
+                                                          "Oracle 7-10g", "FNV-164", "CRC-64"],
+    re.compile(r"^\*[a-f0-9]{40}$", re.IGNORECASE):      ["MySQL5.x", "MySQL4.1"],
+    re.compile(r"^[a-f0-9]{48}$", re.IGNORECASE):        ["Haval-192", "Tiger-192", "SHA-1(Oracle)",
+                                                          "XSHA (v10.4 - v10.6)"]
 }
 
 # Sexy ass banner
