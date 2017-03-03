f1af96852ec51649c8ccc7cf1b11aaa9b05b8c7c
Ekultek <thomas.perkins23@icloud.com>
2017-03-03 09:58:36 -0600

    del (future update)

--- scripts/urlencode.py
+++
@@ -1,8 +0,0 @@
-import urllib
-
-
-def tamper_payload(payload):
-    """ Encode a payload into URL format
-    >>> tamper("SELECT * FROM *")
-    SELECT%20%2A%20FROM%20%2A """
-    return urllib.quote(payload)
