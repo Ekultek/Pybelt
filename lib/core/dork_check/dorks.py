c677e19173339f51a23a595758167e6ca1e8fb21
Ekultek <thomas.perkins23@icloud.com>
2017-05-18 13:09:45 -0500

    edited so it'll run on all OS

--- lib/core/dork_check/dorks.py
+++ lib/core/dork_check/dorks.py
@@ -54,8 +54,8 @@ class DorkScanner(object):
                 match = settings.QUERY_REGEX.match(url)  # Match by regex for anything that has a ?<PARAM>= in it
                 if match:
                     results.write(url + "\n")
-        amount_of_urls = len(open(settings.DORK_SCAN_RESULTS_PATH + "//" + filename + ".txt", 'r').readlines())
+        amount_of_urls = len(open(settings.DORK_SCAN_RESULTS_PATH + "/" + filename + ".txt", 'r').readlines())
         success_rate = ((amount_of_urls // 10) + 1) * 10
-        return "Found a total of {} usable links with query (GET) parameters, urls have been saved to {}//{}.txt. " \
+        return "Found a total of {} usable links with query (GET) parameters, urls have been saved to {}/{}.txt. " \
                "This Dork has a success rate of {}%".format(amount_of_urls, settings.DORK_SCAN_RESULTS_PATH, filename,
                                                             success_rate)
